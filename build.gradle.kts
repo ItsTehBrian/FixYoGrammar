6523e61cacaa614805113680a0af9e653af5c1cc
TehBrian <tehbrian@protonmail.com>
2021-12-21 10:10:28 -0500

    build: use kotlin dsl

---
+++ build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    id("java")
+    id("com.github.johnrengelman.shadow") version "6.1.0"
+}
+
+group = "xyz.tehbrian"
+version = "1.1.0"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+repositories {
+    mavenCentral()
+    maven("https://papermc.io/repo/repository/maven-public/") {
+        name = "papermc"
+    }
+    maven("https://oss.sonatype.org/content/groups/public/") {
+        name = "sonatype"
+    }
+}
+
+dependencies {
+    compileOnly("com.destroystokyo.paper:paper-api:1.16.5-R0.1-SNAPSHOT")
+
+    implementation("com.google.inject:guice:5.0.1")
+    implementation("org.languagetool:language-en:5.2")
+
+    implementation("cloud.commandframework:cloud-paper:1.4.0")
+    implementation("cloud.commandframework:cloud-minecraft-extras:1.4.0")
+
+    implementation("net.kyori:adventure-text-minimessage:4.0.0-SNAPSHOT")
+}
+
+tasks {
+    processResources {
+        expand("version" to project.version)
+    }
+}
