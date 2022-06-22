92f3d6b577a6da9d269ced5cbea81388853aa865
TehBrian <tehbrian@protonmail.com>
2022-06-22 17:00:54 -0400

    build: add tehlib dep

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,16 +18,19 @@ repositories {
     maven("https://papermc.io/repo/repository/maven-public/") {
         name = "papermc"
     }
+    maven("https://repo.thbn.me/releases/") {
+        name = "thbn"
+    }
 }
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.18.2-R0.1-SNAPSHOT")
 
     implementation("com.google.inject:guice:5.1.0")
-    implementation("org.languagetool:language-en:5.7")
-
-    implementation("cloud.commandframework:cloud-paper:1.7.0")
     implementation("cloud.commandframework:cloud-minecraft-extras:1.7.0")
+    implementation("dev.tehbrian:tehlib-paper:0.3.1")
+    implementation("org.languagetool:language-en:5.7")
+    implementation("org.spongepowered:configurate-yaml:4.1.2")
 }
 
 tasks {
