ItsTehBrian/FixYoGrammar

commit c8eeea6e41b841db2d6713de5734e64ad6a301db

use SLF4J logger

src/main/java/xyz/tehbrian/fixyogrammar/FixYoGrammar.java
@@ -11,6 +11,7 @@ import com.google.inject.name.Names;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.checkerframework.checker.nullness.qual.MonotonicNonNull;
 import org.languagetool.JLanguageTool;
 import xyz.tehbrian.fixyogrammar.command.CloudController;
 import xyz.tehbrian.fixyogrammar.config.Config;
@@ -23,16 +24,18 @@ import xyz.tehbrian.fixyogrammar.inject.PluginModule;
 import xyz.tehbrian.fixyogrammar.listener.ChatListener;
 
 import java.io.IOException;
-import java.util.logging.Logger;
+import org.slf4j.Logger;
 
 /**
  * The main class for the FixYoGrammar plugin.
  */
 public final class FixYoGrammar extends JavaPlugin {
 
+    private @MonotonicNonNull Injector injector;
+
     @Override
     public void onEnable() {
-        final Injector injector = Guice.createInjector(
+        this.injector = Guice.createInjector(
                 new PluginModule(this),
                 new LanguageToolModule(),
                 new ConfigModule(),
@@ -40,9 +43,9 @@ public final class FixYoGrammar extends JavaPlugin {
         );
 
         final PluginManager pluginManager = this.getServer().getPluginManager();
-        pluginManager.registerEvents(injector.getInstance(ChatListener.class), this);
+        pluginManager.registerEvents(this.injector.getInstance(ChatListener.class), this);
 
-        final CloudController cloudController = injector.getInstance(CloudController.class);
+        final CloudController cloudController = this.injector.getInstance(CloudController.class);
         try {
             cloudController.init();
         } catch (final Exception e) {
@@ -66,19 +69,22 @@ public final class FixYoGrammar extends JavaPlugin {
         cloudController.registerCommand(base.literal("reload")
                 .meta(CommandMeta.DESCRIPTION, "Reloads the plugin.")
                 .handler(context -> {
-                    injector.getInstance(Key.get(ConfigWrapper.class, Names.named("config"))).load();
-                    injector.getInstance(Key.get(ConfigWrapper.class, Names.named("lang"))).load();
-                    injector.getInstance(Config.class).loadValues();
-                    context.getSender().sendMessage(injector.getInstance(Lang.class).c("reload"));
+                    this.injector.getInstance(Key.get(ConfigWrapper.class, Names.named("config"))).load();
+                    this.injector.getInstance(Key.get(ConfigWrapper.class, Names.named("lang"))).load();
+                    this.injector.getInstance(Config.class).loadValues();
+                    context.getSender().sendMessage(this.injector.getInstance(Lang.class).c("reload"));
                 }));
+    }
 
-        final Logger logger = this.getLogger();
-        logger.info("Loading LanguageTool.. this may take a moment or two.");
-        final JLanguageTool jLanguageTool = injector.getInstance(JLanguageTool.class);
+    public void loadLanguageTool() {
+        final Logger logger = this.injector.getInstance(Logger.class);
+        logger.info("Loading LanguageTool, this may take a moment.");
+
+        final JLanguageTool jLanguageTool = this.injector.getInstance(JLanguageTool.class);
         try {
             jLanguageTool.check("dummy text");
         } catch (final IOException e) {
-            logger.severe("There was an error when checking a message: " + e.getMessage());
+            logger.error("There was an error when checking a message: " + e.getMessage());
             return;
         }
         logger.info("Finished loading LanguageTool!");

src/main/java/xyz/tehbrian/fixyogrammar/command/CloudController.java
@@ -8,10 +8,9 @@ import com.google.inject.Inject;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.checkerframework.checker.nullness.qual.NonNull;
-import xyz.tehbrian.fixyogrammar.inject.PluginLogger;
 
 import java.util.function.Function;
-import java.util.logging.Logger;
+import org.slf4j.Logger;
 
 /**
  * Manages a {@link cloud.commandframework.CommandManager} instance.
@@ -25,7 +24,7 @@ public class CloudController {
 
     @Inject
     public CloudController(
-            final @NonNull @PluginLogger Logger logger,
+            final @NonNull Logger logger,
             final @NonNull JavaPlugin javaPlugin
     ) {
         this.logger = logger;
@@ -49,7 +48,7 @@ public class CloudController {
                     Function.identity()
             );
         } catch (final Exception e) {
-            this.logger.severe("Failed to construct the CommandManager. Something has gone very, very wrong!");
+            this.logger.error("Failed to construct the CommandManager. Something has gone very, very wrong!");
             throw e;
         }
 

src/main/java/xyz/tehbrian/fixyogrammar/config/AbstractConfig.java
@@ -2,7 +2,7 @@ package xyz.tehbrian.fixyogrammar.config;
 
 import org.checkerframework.checker.nullness.qual.NonNull;
 
-import java.util.logging.Logger;
+import org.slf4j.Logger;
 
 /**
  * Loads and holds values from a {@link ConfigWrapper}.

src/main/java/xyz/tehbrian/fixyogrammar/config/Config.java
@@ -2,9 +2,8 @@ package xyz.tehbrian.fixyogrammar.config;
 
 import org.bukkit.configuration.file.FileConfiguration;
 import org.checkerframework.checker.nullness.qual.NonNull;
-import xyz.tehbrian.fixyogrammar.inject.PluginLogger;
 
-import java.util.logging.Logger;
+import org.slf4j.Logger;
 
 public class Config extends AbstractConfig {
 
@@ -14,7 +13,7 @@ public class Config extends AbstractConfig {
 
     public Config(
             final @NonNull ConfigWrapper configWrapper,
-            final @NonNull @PluginLogger Logger logger
+            final @NonNull Logger logger
     ) {
         super(configWrapper, logger);
     }
@@ -28,17 +27,17 @@ public class Config extends AbstractConfig {
 
         this.strict = config.getBoolean("strict", false);
         if (!config.isBoolean("strict")) {
-            logger.warning("Config value strict does not exist. Defaulting to false.");
+            logger.warn("Config value strict does not exist. Defaulting to false.");
         }
 
         this.maxSuggestions = config.getInt("max_suggestions", 3);
         if (!config.isInt("max_suggestions")) {
-            logger.warning("Config value max_suggestions does not exist. Defaulting to 3.");
+            logger.warn("Config value max_suggestions does not exist. Defaulting to 3.");
         }
 
         this.shame = config.getBoolean("shame", true);
         if (!config.isBoolean("shame")) {
-            logger.warning("Config value shame does not exist. Defaulting to true.");
+            logger.warn("Config value shame does not exist. Defaulting to true.");
         }
 
         logger.info("Successfully loaded all values for config.yml!");

src/main/java/xyz/tehbrian/fixyogrammar/config/ConfigWrapper.java
@@ -5,14 +5,12 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.checkerframework.checker.nullness.qual.NonNull;
-import xyz.tehbrian.fixyogrammar.inject.PluginLogger;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import org.slf4j.Logger;
 
 /**
  * A wrapper for a {@link FileConfiguration}, which provides convenience methods
@@ -38,7 +36,7 @@ public class ConfigWrapper {
 
     public ConfigWrapper(
             final @NonNull JavaPlugin javaPlugin,
-            final @NonNull @PluginLogger Logger logger,
+            final @NonNull Logger logger,
             final @NonNull String fileName
     ) {
         this(javaPlugin, logger, new File(javaPlugin.getDataFolder(), fileName));
@@ -82,7 +80,7 @@ public class ConfigWrapper {
         try {
             this.get().save(this.file);
         } catch (final IOException ex) {
-            this.logger.log(Level.SEVERE, "Could not save config to " + this.file.getAbsolutePath(), ex);
+            this.logger.error("Could not save config to " + this.file.getAbsolutePath(), ex);
         }
     }
 

src/main/java/xyz/tehbrian/fixyogrammar/config/Lang.java
@@ -6,7 +6,7 @@ import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.checkerframework.checker.nullness.qual.NonNull;
 
-import java.util.logging.Logger;
+import org.slf4j.Logger;
 
 /**
  * Retrieves {@code String}s from a {@link FileConfiguration}
@@ -62,7 +62,7 @@ public class Lang {
         final String rawValue = this.configWrapper.get().getString(configKey);
 
         if (rawValue == null) {
-            this.logger.severe("Attempted to get message from non-existent config key \"" + configKey + "\"!");
+            this.logger.error("Attempted to get message from non-existent config key \"" + configKey + "\"!");
             throw new IllegalArgumentException("No value found in the config for that given config key.");
         }
 

src/main/java/xyz/tehbrian/fixyogrammar/inject/ConfigModule.java
@@ -10,7 +10,7 @@ import xyz.tehbrian.fixyogrammar.config.Config;
 import xyz.tehbrian.fixyogrammar.config.ConfigWrapper;
 import xyz.tehbrian.fixyogrammar.config.Lang;
 
-import java.util.logging.Logger;
+import org.slf4j.Logger;
 
 public final class ConfigModule extends AbstractModule {
 
@@ -18,7 +18,7 @@ public final class ConfigModule extends AbstractModule {
     @Singleton
     public @Named("config") ConfigWrapper provideConfigWrapper(
             final @NonNull JavaPlugin javaPlugin,
-            final @NonNull @PluginLogger Logger logger
+            final @NonNull Logger logger
     ) {
         final ConfigWrapper configWrapper = new ConfigWrapper(javaPlugin, logger, "config.yml");
         configWrapper.saveDefault();
@@ -30,7 +30,7 @@ public final class ConfigModule extends AbstractModule {
     @Singleton
     public Config provideConfig(
             final @NonNull @Named("config") ConfigWrapper configWrapper,
-            final @NonNull @PluginLogger Logger logger
+            final @NonNull Logger logger
     ) {
         final Config config = new Config(configWrapper, logger);
         config.loadValues();
@@ -42,7 +42,7 @@ public final class ConfigModule extends AbstractModule {
     @Singleton
     public @Named("lang") ConfigWrapper provideLangWrapper(
             final @NonNull JavaPlugin javaPlugin,
-            final @NonNull @PluginLogger Logger logger
+            final @NonNull Logger logger
     ) {
         final ConfigWrapper langWrapper = new ConfigWrapper(javaPlugin, logger, "lang.yml");
         langWrapper.saveDefault();
@@ -54,7 +54,7 @@ public final class ConfigModule extends AbstractModule {
     @Singleton
     public Lang provideLang(
             final @NonNull @Named("lang") ConfigWrapper langWrapper,
-            final @NonNull @PluginLogger Logger logger
+            final @NonNull Logger logger
     ) {
         return new Lang(langWrapper, logger);
     }

src/main/java/xyz/tehbrian/fixyogrammar/inject/PluginLogger.java
@@ -1,17 +1,0 @@
-package xyz.tehbrian.fixyogrammar.inject;
-
-import javax.inject.Qualifier;
-import java.lang.annotation.Retention;
-import java.lang.annotation.Target;
-
-import static java.lang.annotation.ElementType.FIELD;
-import static java.lang.annotation.ElementType.METHOD;
-import static java.lang.annotation.ElementType.PARAMETER;
-import static java.lang.annotation.RetentionPolicy.RUNTIME;
-
-@Qualifier
-@Target({FIELD, PARAMETER, METHOD})
-@Retention(RUNTIME)
-public @interface PluginLogger {
-
-}

commit df3ae36c54d5b9345fbf59519016fe4bf3b31ea5

call #loadLanguageTool in #onEnable (regression in c8eeea6e)

src/main/java/xyz/tehbrian/fixyogrammar/FixYoGrammar.java
@@ -55,6 +55,8 @@ public final class FixYoGrammar extends TehPlugin {
         }
 
         this.registerListeners(this.injector.getInstance(ChatListener.class));
+
+        this.loadLanguageTool();
     }
 
     /**